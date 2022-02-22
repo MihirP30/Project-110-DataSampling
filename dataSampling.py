0a7a48808f8d1e57cc80e77a0e3025424b224f13
MihirP30 <71361209+MihirP30@users.noreply.github.com>
2022-02-21 19:50:17 -0500

    Add files via upload

---
+++ dataSampling.py
@@ -0,0 +1,31 @@
+import plotly.figure_factory as ff
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("medium_data.csv")
+data = df["reading_time"].tolist()
+
+# Finding the means
+def random_means(amount):
+    sample_data = []
+    for i in range(0, amount):
+        index = random.randint(0,len(data))
+        value = data[index]
+        sample_data.append(value)
+    mean = statistics.mean(sample_data)
+    return mean
+
+list_of_means = []
+
+for i in range(0,100):
+    set_of_means= random_means(30)
+    list_of_means.append(set_of_means)
+
+# Output
+df = list_of_means
+fig = ff.create_distplot([df], ["reading_time"], show_hist=False)
+fig.show()
+
+print("Sampling Mean is ", statistics.mean(list_of_means))
